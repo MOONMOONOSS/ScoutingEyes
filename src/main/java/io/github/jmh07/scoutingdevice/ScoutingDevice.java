26b3ecfba5a1fb7f063758d256a10bb11838f75b
Joseph Hnatek <24803352+jmh07@users.noreply.github.com>
2020-05-10 14:03:27 -0500

    Update ScoutingDevice.java
    Added the new scouting device. Created it constructor.

--- src/main/java/io/github/jmh07/scoutingdevice/ScoutingDevice.java
+++ src/main/java/io/github/jmh07/scoutingdevice/ScoutingDevice.java
@@ -1,6 +1,13 @@
 package io.github.jmh07.scoutingdevice;
 
+import org.bukkit.Art;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
 
 public final class ScoutingDevice extends JavaPlugin {
 
@@ -14,4 +21,23 @@ public final class ScoutingDevice extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    private ItemStack createScoutingDevice() {
+        ItemStack scoutDevice = new ItemStack(Material.ENDER_EYE);
+
+        ItemMeta scoutDeviceMeta = scoutDevice.getItemMeta();
+
+        scoutDeviceMeta.setDisplayName(ChatColor.GOLD + "Eye of the Lord");
+
+        ArrayList<String> scoutDeviceLore = new ArrayList<>();
+
+        scoutDeviceLore.add(ChatColor.BLUE + "The Lord blessed this Eye with the power of finding those who are not worthy of living.");
+
+        scoutDeviceMeta.setLore(scoutDeviceLore);
+
+        scoutDevice.setItemMeta(scoutDeviceMeta);
+
+        return scoutDevice;
+    }
+
 }
