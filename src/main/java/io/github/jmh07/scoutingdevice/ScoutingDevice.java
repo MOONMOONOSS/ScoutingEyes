4872b751f8b93b8be875d7c8afb131427dcedc8a
Joseph Hnatek <24803352+jmh07@users.noreply.github.com>
2020-05-10 14:12:03 -0500

    implemented base item for scouting
    Compiled and tested.

--- src/main/java/io/github/jmh07/scoutingdevice/ScoutingDevice.java
+++ src/main/java/io/github/jmh07/scoutingdevice/ScoutingDevice.java
@@ -1,25 +1,41 @@
 package io.github.jmh07.scoutingdevice;
 
-import org.bukkit.Art;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 
-public final class ScoutingDevice extends JavaPlugin {
+public final class ScoutingDevice extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Scouting Device plugin has started!");
+        this.getCommand("scoutdevice").setExecutor(this::onCommand);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player) {
+            Player player = (Player) sender;
+            ItemStack scoutingDevice = createScoutingDevice();
+            player.getInventory().addItem(scoutingDevice);
+        }
+
+        return true;
     }
 
     private ItemStack createScoutingDevice() {
